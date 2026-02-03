888b19930402a82280939f5c8a6eb3f92ad4c9db
Anubhav Rajput <rajanubhav@gmail.com>
2026-02-03 09:18:41 +0530

    Add Pin Configuration In Ultrasonic Sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include <Arduino.h>
 #define trigPin 9
 #define echoPin 10
-void setup() {
 
+void setup()
+{
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
 }
 
-void loop() {
-// write your code here
+void loop()
+{
+    digitalWrite(triPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+
 }
